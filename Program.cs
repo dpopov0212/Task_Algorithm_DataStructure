c4bbd0c5d3e0c2123f0620f76b1d0dc12e9bbdb9
Dmitry Popov <dpopov0212@gmail.com>
2022-05-15 11:33:33 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeTask_Algorithm_DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
